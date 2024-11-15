9bdc0f8ed22ae945395a3b258345c91abf5ad39e
morejuela <maicol.orejuela.asesoftware@gmail.com>
2024-11-15 15:11:22 -0500

    new cascade components

--- init-app-angular/src/app/app.component.ts
+++ init-app-angular/src/app/app.component.ts
@@ -3,11 +3,12 @@ import { RouterOutlet } from '@angular/router';
 import { NewComponentComponent } from "./new-component/new-component.component";
 import { NewComponentInlineComponent } from "./new-component-inline/new-component-inline.component";
 import { InterpolationComponent } from "./interpolation/interpolation.component";
+import { FatherComponent } from "./father/father.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NewComponentComponent, NewComponentInlineComponent, InterpolationComponent],
+  imports: [RouterOutlet, NewComponentComponent, NewComponentInlineComponent, InterpolationComponent, FatherComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ init-app-angular/src/app/father/father.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { DaughterComponent } from "./daughter/daughter.component";
+
+@Component({
+  selector: 'app-father',
+  standalone: true,
+  imports: [DaughterComponent],
+  templateUrl: './father.component.html',
+  styleUrl: './father.component.css'
+})
+export class FatherComponent {
+
+}
