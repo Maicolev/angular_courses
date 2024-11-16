8ac65d59c74aa1179e88caa15a0312b4f8d23aa6
morejuela <maicol.orejuela.asesoftware@gmail.com>
2024-11-15 20:11:53 -0500

    product list

--- init-app-angular/src/app/app.component.ts
+++ init-app-angular/src/app/app.component.ts
@@ -1,17 +1,19 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { NewComponentComponent } from "./new-component/new-component.component";
-import { NewComponentInlineComponent } from "./new-component-inline/new-component-inline.component";
-import { InterpolationComponent } from "./interpolation/interpolation.component";
-import { FatherComponent } from "./father/father.component";
+import { ProductListComponentComponent } from "./product-list-component/product-list-component.component";
+//import { NewComponentComponent } from "./new-component/new-component.component";
+//import { NewComponentInlineComponent } from "./new-component-inline/new-component-inline.component";
+//import { InterpolationComponent } from "./interpolation/interpolation.component";
+//import { FatherComponent } from "./father/father.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NewComponentComponent, NewComponentInlineComponent, InterpolationComponent, FatherComponent],
+  //imports: [RouterOutlet, NewComponentComponent, NewComponentInlineComponent, InterpolationComponent, FatherComponent],
+  imports: [RouterOutlet, ProductListComponentComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'Hi from AppAngular';
+  title = 'Online Store';
 }
