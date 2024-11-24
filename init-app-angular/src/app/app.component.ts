13adc4e09db0a046b8e66d91ef2c1d7f8b5cc023
morejuela <maicol.orejuela.asesoftware@gmail.com>
2024-11-23 20:28:36 -0500

    Conditional

--- init-app-angular/src/app/app.component.ts
+++ init-app-angular/src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { ConditionalsComponent } from "./conditionals/conditionals.component";
+/**
 import { RouterOutlet } from '@angular/router';
 import { ProductListComponentComponent } from "./product-list-component/product-list-component.component";
 import { FatherComponent } from './father/father.component';
@@ -6,19 +8,18 @@ import { EventBindingComponent } from './event-binding/event-binding.component';
 import { PropertyBindingComponent } from "./property-binding/property-binding.component";
 import { TwoWayBindingComponent } from "./two-way-binding/two-way-binding.component";
 import { CalculatorComponent } from "./calculator/calculator.component";
-//import { NewComponentComponent } from "./new-component/new-component.component";
-//import { NewComponentInlineComponent } from "./new-component-inline/new-component-inline.component";
-//import { InterpolationComponent } from "./interpolation/interpolation.component";
-//import { FatherComponent } from "./father/father.component";
+import { NewComponentComponent } from "./new-component/new-component.component";
+import { NewComponentInlineComponent } from "./new-component-inline/new-component-inline.component";
+import { InterpolationComponent } from "./interpolation/interpolation.component";
+**/
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  //imports: [RouterOutlet, NewComponentComponent, NewComponentInlineComponent, InterpolationComponent, FatherComponent],
-  imports: [RouterOutlet, TwoWayBindingComponent, CalculatorComponent],
+  imports: [ConditionalsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'Calculator App';
+  title = 'Test App';
 }
